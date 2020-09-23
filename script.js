939624182b3c22df65d664eb3ea3772130b69b76
Amelbougrine <amel.1989ab@gmail.com>
2020-09-23 23:46:20 +0100

    page 2

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const menu = document.querySelector(".menu")
 function showMenu () {
-    document.querySelector(".menu").classList.toggle("show");
+    menu.classList.toggle("show");
+}
+function nextPage () {
+    document.querySelector(".textContainer").classList.add("slideOut");
+    document.querySelector(".frame").classList.add("slideOut");
+    setTimeout( () => {
+        document.querySelectorAll(".P1").forEach(element => {
+            element.style.display= "none";
+        });
+    }, 2000);
+    document.querySelectorAll(".P2").forEach(element => {
+        element.style.display= "block";
+    });
+    if (menu.classList.contains("show")) {
+        menu.classList.remove("show");
+    }
 }
