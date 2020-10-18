7b54edcaf1f370500c69305feabfd3d8ef1a7bc1
Amelbougrine <amel.1989ab@gmail.com>
2020-10-18 18:57:32 +0100

    menu animations

--- script.js
+++ script.js
@@ -10,17 +10,24 @@ const circle4 = document.querySelector(".circle4");
 const circle5 = document.querySelector(".circle5");
 const circles = [circle1, circle2, circle3, circle4, circle5];
 
+const textContainer = document.querySelector(".textContainer");
+const frame = document.querySelector(".frame");
+const photo = document.querySelector(".photo");
+const blocAnim = document.querySelector(".blocAnim");
+const P1 = document.querySelector(".P1");
+const P2 = document.querySelector(".P2");
+const P3 = document.querySelectorAll(".P3");
+const P4 = document.querySelector(".P4");
+const P5 = document.querySelector(".P5");
+const pages = [P1, P2, P3, P4, P5];
+
 function nextPage2 () {
     numPage = 2;
-    document.querySelector(".textContainer").classList.add("slideOutTop");
-    document.querySelector(".frame").classList.add("slideOutTop");
-    document.querySelectorAll(".P2").forEach(element => {
-        element.style.display= "block";
-    });
+    textContainer.classList.add("slideOutTop");
+    frame.classList.add("slideOutTop");
+    P2.style.display= "block";
     setTimeout( () => {
-        document.querySelectorAll(".P1").forEach(element => {
-            element.style.display = "none";
-        });
+        P1.style.display = "none";
     }, 2000);
     if (menu.classList.contains("show")) {
         menu.classList.remove("show");
@@ -32,16 +39,14 @@ function nextPage2 () {
 }
 function nextPage3 () {
     numPage = 3;
-    document.querySelector(".P2").classList.add("slideOutLeft");
-    document.querySelector(".photo").classList.add("slideoutPhoto");
-    document.querySelectorAll(".P3").forEach(element => {
+    P2.classList.add("slideOutLeft");
+    photo.classList.add("slideoutPhoto");
+    P3.forEach(element => {
         element.style.display= "block";
     });
     setTimeout( () => {
-        document.querySelector(".photo").classList.add("P2");
-        document.querySelectorAll(".P2").forEach(element => {
-            element.style.display= "none";
-        });
+        photo.style.display= "none";
+        P2.style.display= "none";
     }, 1000);
     if (menu.classList.contains("show")) {
         menu.classList.remove("show");
@@ -53,17 +58,15 @@ function nextPage3 () {
 }
 function nextPage4 () {
     numPage = 4;
-    document.querySelectorAll(".P3").forEach(element => {
+    P3.forEach(element => {
         element.classList.add("slideOutTop");
     });
-    document.querySelectorAll(".P4").forEach(element => {
-        element.style.display = "block";
-    });
+    P4.style.display = "block";
     setTimeout( () => {
-        document.querySelectorAll(".P3").forEach(element => {
+        P3.forEach(element => {
             element.style.display = "none";
         });
-        document.querySelector(".blocAnim").style.display = "none";
+        blocAnim.style.display = "none";
     }, 2000);
     if (menu.classList.contains("show")) {
         menu.classList.remove("show");
@@ -75,16 +78,10 @@ function nextPage4 () {
 }
 function nextPage5 () {
     numPage = 5;
-    document.querySelectorAll(".P4").forEach(element => {
-        element.classList.add("slideOutRight");
-    });
-    document.querySelectorAll(".P5").forEach(element => {
-        element.style.display= "block";
-    });
+    P4.classList.add("slideOutRight");
+    P5.style.display= "block";
     setTimeout( () => {
-        document.querySelectorAll(".P4").forEach(element => {
-            element.style.display= "none";
-        });
+        P4.style.display= "none";
     }, 2000);
     if (menu.classList.contains("show")) {
         menu.classList.remove("show");
@@ -93,6 +90,27 @@ function nextPage5 () {
         element.innerHTML = "";
     });
     circle5.innerHTML = "<div></div>";
+}
+function nextPage1 () {
+    numPage = 1;
+    P5.classList.add("slideOutDown");
+    textContainer.classList.remove("slideinLeft");
+    // textContainer.classList.add("slideInDown");
+    P1.classList.add("slideInDown");
+    P1.style.display= "block";
+    photo.classList.remove("slideinPhoto");
+    photo.classList.add("slideInDown");
+    photo.style.display= "block";
+    setTimeout( () => {
+        P5.style.display= "none";
+    }, 1500);
+    if (menu.classList.contains("show")) {
+        menu.classList.remove("show");
+    }
+    circles.forEach(element => {
+        element.innerHTML = "";
+    });
+    circle1.innerHTML = "<div></div>";
 }
 // show project
 const myWorkIntro = document.querySelector(".myWorkIntro");
@@ -109,4 +127,39 @@ function showProject(num) {
         element.style.display= "none";
     });
     detailArray[num-1].style.display = "block";
+}
+// show page
+function Page(num) {
+    for (let i=0; i<pages.length; i++) {
+        if (i==2) {
+            pages[i].forEach(element => {
+                element.style.display= "none";
+            });
+        } else {
+            pages[i].style.display= "none";
+        }
+        console.log(pages[i]);
+    };
+    photo.style.display = "none";
+
+    if (num == 3) {
+        pages[num-1].forEach(element => {
+            element.classList.remove("slideinright");
+            element.classList.add("zoomIn");
+            element.style.display = "block";
+        });
+    } else {
+        pages[num-1].classList.remove("slideUp665", "slideleft", "slideInUp", "slideOutDown", "slideInDown");
+        pages[num-1].classList.add("zoomIn");
+        pages[num-1].style.display = "block";
+    }
+    if(num == 1 || num == 2) {
+        photo.classList.remove("slideinPhoto");
+        photo.classList.add("zoomIn");
+        photo.style.display = "block";
+    }
+    if(num == 4) {
+        blocAnim.style.display = "none";
+    }
+    showMenu();
 }
